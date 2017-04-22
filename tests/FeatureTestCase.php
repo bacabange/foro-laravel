b94fa5d7327abae8d57218d7a0d852a782800178
Stiven <stivencastillo.90@gmail.com>
2017-04-22 08:34:32 +0000

    Refactorization see errors in test case

--- tests/FeatureTestCase.php
+++ tests/FeatureTestCase.php
@@ -7,5 +7,15 @@ class FeatureTestCase extends TestCase
 {
 
 	use DatabaseTransactions;
+
+	public function seeErrors(array $fields)
+	{
+		foreach ($fields as $name => $errors) {
+			foreach ((array) $errors as $message) {
+        		$this->seeInElement("#field_{$name} .help-block", $message);
+			}
+		}
+
+	}
 	
 }
