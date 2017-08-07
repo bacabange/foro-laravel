cc06fa23c43881e27a3d01cf298a7b7af2339ee5
vagrant <vagrant@homestead>
2017-08-07 18:07:47 +0000

    Posts list paginated, test for pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,13 @@ use App\Post;
 
 class PostController extends Controller
 {
+
+	public function index()
+	{
+		$posts = Post::orderBy('created_at', 'DESC')->paginate();
+		return view('posts.index', compact('posts'));
+	}
+
     public function show (Post $post, $slug) 
     {
     	// abort_if($post->slug != $slug, 404);
